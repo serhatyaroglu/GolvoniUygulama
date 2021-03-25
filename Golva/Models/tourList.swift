be6778b7e956162a17d5cc6f26cd51c1e98afc3f
serhatyaroglu <61588155+serhatyaroglu@users.noreply.github.com>
2021-03-25 18:27:51 +0300

    s

--- Golva/Models/tourList.swift
+++ Golva/Models/tourList.swift
@@ -10,8 +10,8 @@ class tourModel : Codable  {
     var name : String?
     var parkourName : String?
     var fee : Int
-    var totalCustomerCount : Int
-    var startDate : Date
+//    var totalCustomerCount : Int
+//    var startDate : Date
 }
 class mainEvent: Codable {
     var weekCount : Int?
