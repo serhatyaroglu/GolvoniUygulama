80f9380411c735b0dbfe618e99cd82014633a4cd
serhatyaroglu <61588155+serhatyaroglu@users.noreply.github.com>
2021-04-06 09:12:27 +0300

    callapi

--- Golva/ViewControllers/ProfileViewController.swift
+++ Golva/ViewControllers/ProfileViewController.swift
@@ -6,16 +6,34 @@
 //
 
 import UIKit
-
+import Alamofire
 class ProfileViewController: UIViewController {
-
+    @IBOutlet weak var UIView: UIView!
+    @IBOutlet weak var PersonPhoto: UIImageView!
+    @IBOutlet weak var PersonName: UILabel!
+    @IBOutlet weak var JobOrCompany: UILabel!
+    var ApiProfile = [tourModel]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        callTourApi()
+        let  currentapi = ApiProfile
+        PersonName.text = currentapi.
+    }
+    func callTourApi(){
+        AF.request(URL(string: "http://golvoni.com/api/tours/guide")!,method: .get,parameters: nil, headers: nil).responseJSON { (response) in
+            if let responseData = response.data{
+                do {
+                    let decodeJSON = JSONDecoder()
+                    decodeJSON.keyDecodingStrategy = .convertFromSnakeCase
+                    self.ApiProfile = try decodeJSON.decode([tourModel].self, from: responseData)
+//                    self.UIView.reloadData()
+                }catch{
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
     
-
   
 
 }
